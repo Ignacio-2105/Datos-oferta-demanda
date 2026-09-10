c2f09cdf4b75bc95b31cf00a655e440af49d0e3b
Ignacio-2105 <ihernandezma@usm.cl>
2026-09-10 20:33:44 -0300

    Ponderación preferencias / año

---
+++ Codes/Ponderación carreras 2025.R
@@ -0,0 +1,61 @@
+library(readr)
+library(readxl)
+library(tidyverse)
+
+# Escribir directorio donde están guardados los archivos. Caso Ignacio
+Ruta <- "/home/ignacio/Documents/Tareas USM/Proyecto modelación MAT282/Datos/datos demre 2025/"
+
+#Matricula <- read_delim(paste0(Ruta, "Matricula/ArchivoMatr_Adm2025.csv"), 
+#                             delim = ";", escape_double = FALSE, locale = locale(decimal_mark = ",", 
+#                                                                                 grouping_mark = "."), trim_ws = TRUE) 
+
+ArchivoD <- read_delim(paste0(Ruta, "Postulacion/PostulaciónySelección_Admisión2025/ArchivoD_Adm2025.csv"), 
+                            delim = ";", escape_double = FALSE, locale = locale(decimal_mark = ",", 
+                                                                                grouping_mark = "."), trim_ws = TRUE)
+
+INDICADORES_POR_CARRERA_PROMEDIO_OBLIGATORIAS <- read_delim(paste0(Ruta, "Postulacion/ADM2025_INDICADORES_POR_CARRERA_PROMEDIO_OBLIGATORIAS_20250120.csv"), 
+                                                                 delim = ";", escape_double = FALSE, locale = locale(), 
+                                                                 trim_ws = TRUE)
+
+#Libro_CódigosADM_ArchivoMatricula <- read_excel(paste0(Ruta, "Matricula/Libro_CódigosADM2025_ArchivoMatricula.xlsx"))
+
+# ¿Cómo mejor distinguirlos?
+#OfertaAcadémica <- read_excel(paste0(Ruta, "Postulacion/OfertaAcadémica_Admisión2025.xlsx"))
+
+#OfertaAcadémica2 <- read_excel(paste0(Ruta, "Postulacion/PostulaciónySelección_Admisión2025 (archivo D)/Libro_CódigosADM2025_ArchivoD.xlsx"), 
+#                                    sheet = "Anexo -  Oferta académica")
+
+#Libro_CódigosADM2025_ArchivoD <- read_excel(paste0(Ruta, "Postulacion/PostulaciónySelección_Admisión2025 (archivo D)/Libro_CódigosADM2025_ArchivoD.xlsx"))
+
+#Libro_CódigosADM2025_IndicadoresCarreraPromedioObligatorias <- read_excel(paste0(Ruta, "Postulacion/Estadistica de seleccion/Libro_CódigosADM2025_IndicadoresCarreraPromedioObligatorias.xlsx"), 
+#                                                                          sheet = "IndicadoresPromedioObligatorias")
+
+# Toma todas las preferencias de una carrera, 
+# y pondera de acuerdo a qué tan alta es la preferencia.
+
+# Se planea dividir por el total de estudiantes
+
+ponderacion <- function(n) {
+  poder <- 0
+  for (i in n) {
+    # Exponencial inversa
+    poder <- poder + exp(-i + 1)
+  }
+  return (poder)
+}
+
+
+# Lee la oferta académica, filtra por fila, y hace una tabla
+# Oferta académica
+oferta = INDICADORES_POR_CARRERA_PROMEDIO_OBLIGATORIAS %>%
+  dplyr::select(CODIGO_CARRERA,NOMBRE_CARRERA)
+postulaciones = ArchivoD %>%
+  # Para el cálculo solo se toman en cuenta las primeras 3 preferencias
+  dplyr::filter(TIPO_PREF == "REGULAR" & ORDEN_PREF <= 3) %>%
+  dplyr::left_join(oferta, by = c("COD_CARRERA_PREF" = "CODIGO_CARRERA")) %>%
+  dplyr::group_by(NOMBRE_CARRERA) %>%
+  dplyr::summarise(Puntaje = ponderacion(ORDEN_PREF)) %>%
+  dplyr:: arrange(desc(Puntaje)) %>%
+  head(10)
+postulaciones %>%
+  knitr::kable(format.args = list(big.mark = ","))
